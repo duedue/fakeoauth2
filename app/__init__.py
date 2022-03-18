0567ca67da3ee45f9358545f129af16c7b8700de
Michael Tsai <michaeltsai@qnap.com>
2022-03-18 18:19:44 +0800

    Init flask service

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# app/__init__.py
+import os
+from datetime import datetime
+
+from flask import Flask
+from flask import current_app as app
+from flask_restful import Api
+
+from .config import config
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    app.secret_key = 'fakeoauth2'
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# config.py
+
+
+class Config(object):
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
+
